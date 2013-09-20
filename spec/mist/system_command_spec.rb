2c26dee8e2dc3c77049b53a3db94732446942a36
Alex Falkowski <alexrfalkowski@gmail.com>
2013-09-20 12:39:07 -0400

    Make sure we pass dependencies as named parameters to SystemCommand.

--- spec/mist/system_command_spec.rb
+++ spec/mist/system_command_spec.rb
@@ -4,14 +4,14 @@ describe Mist::Website do
   context 'using backticks' do
     context 'with parameters' do
       Given(:kernel) { double('Kernel', :` => nil) }
-      Given(:system_command) { Mist::SystemCommand.new(kernel) }
+      Given(:system_command) { Mist::SystemCommand.new(kernel: kernel) }
       When { system_command.run_command_with_output('test', 'test') }
       Then { expect(kernel).to have_received(:`).with('test test') }
     end
 
     context 'without parameters' do
       Given(:kernel) { double('Kernel', :` => nil) }
-      Given(:system_command) { Mist::SystemCommand.new(kernel) }
+      Given(:system_command) { Mist::SystemCommand.new(kernel: kernel) }
       When { system_command.run_command_with_output('test') }
       Then { expect(kernel).to have_received(:`).with('test') }
     end
@@ -20,14 +20,14 @@ describe Mist::Website do
   context 'using system' do
     context 'with parameters' do
       Given(:kernel) { double('Kernel', system: nil) }
-      Given(:system_command) { Mist::SystemCommand.new(kernel) }
+      Given(:system_command) { Mist::SystemCommand.new(kernel: kernel) }
       When { system_command.run_command('test', 'test') }
       Then { expect(kernel).to have_received(:system).with('test test') }
     end
 
     context 'without parameters' do
       Given(:kernel) { double('Kernel', system: nil) }
-      Given(:system_command) { Mist::SystemCommand.new(kernel) }
+      Given(:system_command) { Mist::SystemCommand.new(kernel: kernel) }
       When { system_command.run_command('test') }
       Then { expect(kernel).to have_received(:system).with('test') }
     end
